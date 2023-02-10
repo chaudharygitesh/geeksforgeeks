49305677ca041b23536f813169fef10aabd0b2dc
Gitesh <73160504+chaudharygitesh@users.noreply.github.com>
2023-02-10 20:11:31 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Balloon Everywhere/balloon-everywhere.cpp
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    int maxInstance(string s){
+            unordered_map<char,int> m;
+        for(char c:s)
+        m[c]++;
+        return min({m['b'],m['a'],m['l']/2,m['o']/2,m['n']});
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        Solution ob;
+        cout<<ob.maxInstance(s)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
