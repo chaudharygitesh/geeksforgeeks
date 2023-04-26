1b8c7bf0dd8f4ee8e1c3258b6eb0053fab70a2fa
Gitesh <73160504+chaudharygitesh@users.noreply.github.com>
2023-04-26 22:59:26 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Seating Arrangement/seating-arrangement.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    bool is_possible_to_get_seats(int n, int m, vector<int>& seats){
+        // Write your code here.
+         int avs=0;
+        int c=0;
+        for(int i=0;i<m;i++){
+            if(seats[i]==1){
+                avs+=c/2;
+                c=-1;
+            }
+            else{
+                c++;
+            }
+        }
+        avs+=(c+1)/2;
+        return avs>=n;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int m;
+        cin >> m;
+        vector<int> seats(m);
+        for (int i = 0; i < m; i++) {
+            cin >> seats[i];
+        }
+        Solution obj;
+        if (obj.is_possible_to_get_seats(n, m, seats)) {
+            cout << "Yes" << endl;
+        }
+        else {
+            cout << "No" << endl;
+        }
+    }
+}
+
+// } Driver Code Ends
